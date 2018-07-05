57c5ba0a1c9d4122d3010041fe900537d2e33376
CN_SZTL <22235437+1715173329@users.noreply.github.com>
2018-07-05 05:15:21 -0500

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM debian:sid
+
+RUN apt update -y \
+	&& apt upgrade -y \
+	&& apt install -y wget curl ntpdate unzip lsof cron
+
+ADD entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh 
+CMD /entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,102 @@
+#! /bin/bash
+
+function check_time(){
+	rm -rf /etc/localtime
+	cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+	ntpdate time.nist.gov
+}
+
+function install_v2ray_caddy(){
+	bash <(curl https://install.direct/go.sh)
+	bash <(curl https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/caddy_install.sh)
+}
+
+function set_v2ray_caddy(){
+	if [[ -z "${UserUUID}" ]];then
+		UserUUID="c120a2df-c37b-4e73-b0cf-dd29946dabed"
+	fi
+	if [[ -z "${AlterID}" ]];then
+		AlterID="10"
+	fi
+	if [[ -z "${Path}" ]];then
+		Path="/letscrosschinagfw"
+	fi
+	echo -e "{\"inbound\":{\"listen\":\"127.0.0.1\",\"port\":10000,\"protocol\":\"vmess\",\"settings\":{\"udp\":true,\"clients\":[{\"id\":\"" > /etc/v2ray/config.json
+	echo -e -n "${UserUUID}" > /etc/v2ray/config.json
+	echo -e -n "\",                     \"alterId\": " > /etc/v2ray/config.json
+	echo -e -n "${AlterID}" > /etc/v2ray/config.json
+	echo -e -n "}]},\"streamSettings\":{\"network\":\"ws\",\"wsSettings\":{\"path\":\"" > /etc/v2ray/config.json
+	echo -e -n "${Path}" > /etc/v2ray/config.json
+	echo -e -n "\" 			}         }     },     \"outbound\": {         \"protocol\": \"freedom\",         \"settings\": {}     },     \"inboundDetour\": [],     \"outboundDetour\": [         {             \"protocol\": \"blackhole\",             \"settings\": {},             \"tag\": \"blocked\"         }     ],     \"routing\": {         \"strategy\": \"rules\",         \"settings\": {             \"rules\": [                 {                     \"type\": \"field\",                     \"ip\": [                         \"0.0.0.0/8\",                         \"10.0.0.0/8\",                         \"100.64.0.0/10\",                         \"127.0.0.0/8\",                         \"169.254.0.0/16\",                         \"172.16.0.0/12\",                         \"192.0.0.0/24\",                         \"192.0.2.0/24\",                         \"192.168.0.0/16\",                         \"198.18.0.0/15\",                         \"198.51.100.0/24\",                         \"203.0.113.0/24\",                         \"::1/128\",                         \"fc00::/7\",                         \"fe80::/10\"                     ],                     \"outboundTag\": \"blocked\"                 }             ]         }     } } " > /etc/v2ray/config.json
+	echo -e "localhost:${PORT}
+{
+	root /www
+	timeouts none
+	proxy ${Path} localhost:10000 {
+		websocket
+		header_upstream -Origin
+	}
+}" > /usr/local/caddy/Caddyfile
+}
+
+function set_webindex(){
+	cat <<-EOF > /www/index.html
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>WorldCDN - Designed by World.Team - herokuapp.com</title>
+        <link rel="stylesheet" href="https://fonts.cat.net/css?family=Roboto:300">
+        <style>
+            html, body {
+                height: 100%;
+            }
+            body {
+                margin: 0;
+                padding: 0;
+                width: 100%;
+                display: table;
+                font-weight: 100;
+                font-family: "Roboto",sans-serif;
+            }
+            .container {
+                text-align: center;
+                display: table-cell;
+                vertical-align: middle;
+            }
+            .content {
+                text-align: center;
+                display: inline-block;
+            }
+            .title {
+                font-size: 96px;
+            }
+            .textcontent {
+                font-size: 50px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                <div class="title">WorldCDN for Video</div>
+				<br />
+				<div class="textcontent">Designed by World.Team</div>
+            </div>
+        </div>
+    </body>
+</html>
+	EOF
+}
+
+function restart_service(){
+	service v2ray restart
+	service caddy restart
+}
+	
+function main(){
+	check_time
+	install_v2ray_caddy
+	set_v2ray_caddy
+	set_webindex
+	restart_service
+}
