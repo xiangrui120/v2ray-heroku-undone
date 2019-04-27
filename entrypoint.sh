a61338496befa6a664f6d700562ed936b0fb30b8
mnixry <admin@mnixry.cn>
2019-04-27 15:09:34 +0800

    Fix Some Bugs

--- entrypoint.sh
+++ entrypoint.sh
@@ -34,6 +34,7 @@ date -R
 SYS_Bit="$(getconf LONG_BIT)"
 [[ "$SYS_Bit" == '32' ]] && BitVer='_linux_386.tar.gz'
 [[ "$SYS_Bit" == '64' ]] && BitVer='_linux_amd64.tar.gz'
+echo "您正在使用${SYS_Bit}位系统!"
 
 if [ "$VER" = "latest" ]; then
   V_VER=`wget -qO- "https://api.github.com/repos/v2ray/v2ray-core/releases/latest" | grep 'tag_name' | cut -d\" -f4`
@@ -46,7 +47,7 @@ cd /v2raybin
 wget --no-check-certificate -qO 'v2ray.zip' "https://github.com/v2ray/v2ray-core/releases/download/$V_VER/v2ray-linux-$SYS_Bit.zip"
 unzip v2ray.zip
 rm -rf v2ray.zip
-chmod +x /v2raybin/v2ray-$V_VER-linux-$SYS_Bit/*
+chmod +x /v2ray/*
 
 C_VER=`wget -qO- "https://api.github.com/repos/mholt/caddy/releases/latest" | grep 'tag_name' | cut -d\" -f4`
 mkdir /caddybin
@@ -63,7 +64,7 @@ chmod +x caddy
 # tar xvf demo.tar.gz
 # rm -rf demo.tar.gz
 
-cat <<-EOF > /v2raybin/v2ray-$V_VER-linux-$SYS_Bit/config.json
+cat <<-EOF > /v2ray/config.json
 {
     "log":{
         "loglevel":"warning"
@@ -111,7 +112,7 @@ http://0.0.0.0:${PORT}
 }
 EOF
 
-cat <<-EOF > /v2raybin/vmess.json 
+cat <<-EOF > /v2ray/vmess.json 
 {
     "v": "2",
     "ps": "${AppName}.herokuapp.com",
